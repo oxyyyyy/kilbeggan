b0e1909de6cd3162ea9e308277b03882bc450bd0
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-11-22 16:38:15 +0200

    without loop

--- src/js/common.js
+++ src/js/common.js
@@ -14,63 +14,28 @@ new WOW().init();
 
 // New universal script
 $(document).ready(function () {
+	numberOfVideos = $(".video").length;
+	console.log(numberOfVideos);
 	var video = $(".video").attr("id");
 	var videoSrc = $(".video").attr("src");
 	var videoPreview = $(".video-preview").attr("id");
+	var videoPlayBtn = $(".play-btn").attr("id");
 	var videoCloseBtn = $(".close-btn-container").attr("id");
-	console.log(video);
-	console.log(videoSrc);
-	console.log(videoPreview);
-	console.log(videoCloseBtn);
-	$(videoPreview).click(function () {
+	$("#"+videoPreview).click(function () {
 		$(this).hide();
-		$(video)[0].videoSrc += "&autoplay=1";
-		$(video).show();
-		$(videoCloseBtn).show();
+		$("#"+video)[0].videoSrc += "&autoplay=1";
+		$("#"+video).show();
+		$("#"+videoCloseBtn).show();
 	});
-	$(videoCloseBtn).click(function () {
+	$("#"+videoCloseBtn).click(function () {
 		$(this).hide();
-		$(videoCloseBtn).hide();
-		$(videoPreview).show();
-		$(video).hide();
-		$(video).attr("src", "");
-		$(video).attr("src", videoSrc); 
+		$("#"+videoCloseBtn).hide();
+		$("#"+videoPreview).show();
+		$("#"+video).hide();
+		$("#"+video).attr("src", "");
+		$("#"+video).attr("src", videoSrc); 
 	});
 })
-
-// Video 1
-// var video1 = $("#video-1").attr("src");
-// $("#video-preview-1").click(function () {
-// 	$(this).hide();
-// 	$("#video-1")[0].src += "&autoplay=1";
-// 	$("#video-1").show();
-// 	$("#close-btn-1").show();
-// });
-// $("#close-btn-1").click(function () {
-// 	$(this).hide();
-// 	$("#close-btn-1").hide();
-// 	$("#video-preview-1").show();
-// 	$("#video-1").hide();
-// 	$("#video-1").attr("src", "");
-// 	$("#video-1").attr("src", video1);
-// });
-
-// Video 2
-// var video2 = $("#video-2").attr("src");
-// $("#video-preview-2").click(function () {
-// 	$(this).hide();
-// 	$("#video-2")[0].src += "&autoplay=1";
-// 	$("#video-2").show();
-// 	$("#close-btn-2").show();
-// });
-// $("#close-btn-2").click(function () {
-// 	$(this).hide();
-// 	$("#close-btn-2").hide();
-// 	$("#video-preview-2").show();
-// 	$("#video-2").hide();
-// 	$("#video-2").attr("src", "");
-// 	$("#video-2").attr("src", video2);
-// });
 
 // Video 3
 // var video3 = $("#video-3").attr("src");
