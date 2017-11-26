bfb6af2aeb78c7f01287d834c614541ec7e738c9
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-11-26 16:48:58 +0200

    fixed "products" (or not?..)

--- dist/js/common.js
+++ dist/js/common.js
@@ -9,56 +9,27 @@ $("#index_header_slider").glide({
 // Init wow.js
 new WOW().init();
 
-
 // Videos
-
-// Video 1
-var video1 = $("#video-1").attr("src");
-$("#video-preview-1").click(function () {
-	$(this).hide();
-	$("#video-1")[0].src += "&autoplay=1";
-	$("#video-1").show();
-	$("#close-btn-1").show();
-});
-$("#close-btn-1").click(function () {
-	$(this).hide();
-	$("#close-btn-1").hide();
-	$("#video-preview-1").show();
-	$("#video-1").hide();
-	$("#video-1").attr("src", "");
-	$("#video-1").attr("src", video1);
-});
-
-// Video 2
-var video2 = $("#video-2").attr("src");
-$("#video-preview-2").click(function () {
-	$(this).hide();
-	$("#video-2")[0].src += "&autoplay=1";
-	$("#video-2").show();
-	$("#close-btn-2").show();
-});
-$("#close-btn-2").click(function () {
-	$(this).hide();
-	$("#close-btn-2").hide();
-	$("#video-preview-2").show();
-	$("#video-2").hide();
-	$("#video-2").attr("src", "");
-	$("#video-2").attr("src", video2);
-});
-
-// Video 3
-var video3 = $("#video-3").attr("src");
-$("#video-preview-3").click(function () {
-	$(this).hide();
-	$("#video-3")[0].src += "&autoplay=1";
-	$("#video-3").show();
-	$("#close-btn-3").show();
-});
-$("#close-btn-3").click(function () {
-	$(this).hide();
-	$("#close-btn-3").hide();
-	$("#video-preview-3").show();
-	$("#video-3").hide();
-	$("#video-3").attr("src", "");
-	$("#video-3").attr("src", video3);
+$(document).ready(function () {
+	const initVideoPlayer =
+		id => {
+			const src = $(`#video-${id}`).attr('src');
+			$(`#video-preview-${id}`).click(function() {
+				$(this).hide();
+				$(`#video-${id}`)[0].src += '&autoplay=1';
+				$(`#video-${id}`).show();
+				$(`#close-btn-${id}`).show();
+			});
+			$(`#close-btn-${id}`).click(function () {
+				$(this).hide();
+				$(`#close-btn-${id}`).hide();
+				$(`#video-preview-${id}`).show();
+				$(`#video-${id}`).hide();
+				$(`#video-${id}`).attr('src', '');
+				$(`#video-${id}`).attr('src', src);
+			});
+		}
+		$(".video").each(function () {
+			initVideoPlayer(this.id.substr(this.id.length-1,1));
+		});
 });
